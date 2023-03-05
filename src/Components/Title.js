17b6914ca7eb2e5cf33984884579329b67e04fa0
chocoboy08 <chocoboy0508@gmail.com>
2023-03-05 18:46:00 +0900

    MyRecipe-mui

---
+++ src/Components/Title.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Title() {
+  return (
+    <Typography variant="h2" align="center" fontSize="9vw" fontWeight="bold">
+      My Recipes
+    </Typography>
+  );
+}
